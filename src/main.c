e59e62e1bbaa48aab8ba8662c97d655a886275c8
Omar Aglan <Omar.aglan91@gmail.com>
2023-03-24 05:40:56 +0200

    tas;

--- src/main.c
+++ src/main.c
@@ -1,8 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-
-/// @brief 
-/// @param  
-/// @return 
-int main(void){
+fpos_t file_size(FILE *file)
+{
+    if (!file)
+    {
+        /* code */
+        return 0;
+    }
+    fpos_t out = 0;
+    assert(fgetpos(file, out) == 0 && "لا يمكن الوصول لموقع الملف");
+    return out;
+}
+/// @brief Gets File Content
+/// @param path 
+/// @return File Content In Char
+char *File_Content(char *path)
+{
+   FILE *file = fopen(path, "r");
+   if (!file)
+   {
+    printf("لا يمكن قراءة الملف في المسار %s", path);
+    return NULL;
+   }
+   fpos_t size = file_size(file);
+}
+/// @brief Prints Usage Of Arguments
+/// @param argv Argument Char
+void print_argUsage(char **argv)
+{
+    printf("الإستخدام: %s <الملف_المراد_تجميعه>\n", argv[0]);
+    
+}
+/// @brief The Main Method - or Starting Method 
+/// @param argc Numbers Of Arguments
+/// @param argv Argument Char
+/// @return 0 for Success Or 1 for Failure 
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        print_argUsage(argv);
+        exit(0);
+    }
+    
     return 0;
 }
